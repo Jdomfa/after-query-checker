c5e6a839cddef851d4f70ac12e4da6faefa97ed1
Joshua Domfa Johnson <jjoshuadomfa@gmail.com>
2026-06-20 13:29:15 +0100

    Update afterquery_watcher.py

--- afterquery_watcher.py
+++ afterquery_watcher.py
@@ -30,7 +30,7 @@ log = logging.getLogger(__name__)
 
 auth_token = None
 session = requests.Session()
-PROXY = "http://frkdlotu:rvnp8eo0otno@23.95.150.145:6114"
+PROXY = os.environ.get("PROXY_URL")
 session.proxies = {
     "http": PROXY,
     "https": PROXY,
